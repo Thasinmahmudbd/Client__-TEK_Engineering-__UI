a94ad0aba3a1e98479de8a8f3ff6a2d1023ad078
Thasin Mahmud <thasinmahmud.bd@gmail.com>
2021-04-28 18:00:14 +0600

    Newsroom created, linked with index, made responsive.

--- Script/onscroll.js
+++ Script/onscroll.js
@@ -1,7 +1,7 @@
 window.onscroll = function() {myFunction()};
 
 function myFunction() {
-  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+  if (document.body.scrollTop > 10 || document.documentElement.scrollTop > 10) {
     document.getElementById("ancor").className = "header_on_scroll";
   } else {
     document.getElementById("ancor").className = "header";
